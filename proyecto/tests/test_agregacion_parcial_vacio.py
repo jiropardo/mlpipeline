cfa28e432fa60f0419962ad904d30cb967846662
jiropardo <jairo.prado@databricks.com>
2025-11-09 16:34:54 -0600

    t

--- proyecto/tests/test_agregacion_parcial_vacio.py
+++
@@ -1,8 +0,0 @@
-from programaestudiante.funciones.procesamiento import agregacion_parcial
-
-def test_agregacion_parcial_vacio(spark):
-    df_vacio = spark.createDataFrame([], "cedula INT, provincia STRING, fecha STRING, kilometros DOUBLE")
-
-    df_agg = agregacion_parcial(df_vacio)
-
-    assert df_agg.count() == 0
