82f4ae9420f1d523983dfe9ee8e33d41f6624c89
jiropardo <jairo.prado@databricks.com>
2025-11-09 23:15:44 -0600

    t

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ def main():
     
     spark = SparkSession.builder \
     .appName("Programa Estudiante") \
-    .config("spark.jars", "/src/sparkml/postgresql-42.2.14.jar") \
     .config("spark.driver.extraClassPath", "/src/sparkml/postgresql-42.2.14.jar") \
     .config("spark.executor.extraClassPath", "/src/sparkml/postgresql-42.2.14.jar") \
     .getOrCreate()
